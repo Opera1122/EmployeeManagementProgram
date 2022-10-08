67167cc6b936350ab98e0de8d61adf71cbceb40b
gl003134 <gl003134@gmail.com>
2022-10-08 23:33:39 +0900

    update : 로그인 에러창 GUI 수정

--- src/LoginError.java
+++ src/LoginError.java
@@ -7,17 +7,18 @@ import java.awt.event.ActionListener;
 public class LoginError extends JFrame {
     JLabel label = new JLabel("아이디 또는 비밀번호를 다시 확인하세요.");
     JButton checkButton = new JButton("확인");
-    JPanel panel = new JPanel();
     public LoginError() {
         setTitle("로그인 실패");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(500, 200);
+        setSize(500, 140);
+        setLayout(null);
         setResizable(false);
         setVisible(true);
 
-        add(panel);
-        panel.add(label);
-        panel.add(checkButton);
+        add(label);
+        label.setBounds(125, 10, 400, 30);
+        add(checkButton);
+        checkButton.setBounds(200, 50, 80, 30);
         checkButton.addActionListener(new LoginError.MyListener());
     }
 
