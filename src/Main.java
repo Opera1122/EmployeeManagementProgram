84b45373374f2219312a81cd92ab0e7b53c74e94
gl003134 <gl003134@gmail.com>
2022-10-09 21:46:04 +0900

    update : Employee DTO 클래스에 position을 추가

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class Main extends JFrame {
     public static void main(String[] args) {
-        Login lg = new Login();
-//        MenuSelection m = new MenuSelection();
+//        Login lg = new Login();
+        MenuSelection m = new MenuSelection();
     }
 }
